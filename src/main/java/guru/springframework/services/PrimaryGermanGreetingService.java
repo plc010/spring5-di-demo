354b5b6f4d68971ebd1d850c6df5a3bd670aa26e
plc010 <shaker72@icloud.com>
2018-07-05 10:38:14 -0400

    Implemented java config of factory class

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,20 +1,13 @@
 package guru.springframework.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
 /**
  * Created by jt on 5/24/17.
  */
-@Service
-@Primary
-@Profile("de")
 public class PrimaryGermanGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
 
-    public PrimaryGermanGreetingService(GreetingRepository greetingRepository) {
+    PrimaryGermanGreetingService(GreetingRepository greetingRepository) {
         this.greetingRepository = greetingRepository;
     }
 
